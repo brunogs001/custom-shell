8398d904f205ca574a1eba375d3a6bc3599bee88
Bruno Silva <39249775+brunogs001@users.noreply.github.com>
2025-10-26 21:29:28 -0300

    Enhance install script for ZShell and plugins
    Added checks for hostname and shell, installed ZShell, and added plugins for ZSH.

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
+#!/bin/bash
+set -e
+
+## Check hostname and shell before starting the installation 
+if [ "$HOSTNAME" != "desk" || "$SHELL" != "/bin/bash" ]; then
+    exit 1
+
+## Install ZShell as a requirement to OhMyZSH
+sudo apt update; sudo apt install zsh -y
+
+## Change Shell to ZShell
+chsh -s `which zsh`
+## PASSWORD????
+
 ## Install ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 ## Install Peltoche LSD
-apt install lsd
+sudo apt install lsd -y
 
 ## Install Hack Nerd Fonts
 mkdir -p ~/.local/share/fonts
@@ -15,6 +29,18 @@ echo 'eval "$(starship init zsh)"' >> ~/.zshrc
 
 ## Install ZSH Auto Suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+## Install Syntax Highlight
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+echo "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
+## Add .config files
+## Create the config folder if not exist
+if [ ! -d "${HOME}/.config" ]; then
+    mkdir "${HOME}/.config"
+    cp .config/startship.toml ${HOME}/.config/starship.toml
+    mkdir "${HOME}/.config/lsd"
+    cp .config/lsd/config.yaml ${HOME}/.config/lsd/config.yaml
 
 ## Enable auto suggestions in .zshrc
 #plugins=( 
